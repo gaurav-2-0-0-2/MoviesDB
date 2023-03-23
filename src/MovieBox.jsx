5e0987b8bc49f94d11272e4643a7cf5d01a7ee22
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-03-23 11:53:12 +0530

    styles added to movie cards

--- src/MovieBox.jsx
+++ src/MovieBox.jsx
@@ -5,11 +5,11 @@ const MovieBox = ({ title, poster_path, overview, release_date }) => {
 
     return (
 
-        <div>
-            <img className="h-[450px] w-full" src={imageUrl + poster_path} alt="1" />
-            <h1>{title}</h1>
+        <div className="bg-white p-4">
+            <img className="h-[450px] w-full mb-4" src={imageUrl + poster_path} alt="1" />
+            <h1 className="text-2xl font-bold">{title}</h1>
             <p>{overview}</p>
-            <p>{release_date}</p>
+            <p className="font-bold">Release Date:{release_date}</p>
         </div>
 
     );
