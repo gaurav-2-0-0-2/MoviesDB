9809ad0fcff6772385efbd3a08f2c0713e77646b
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-06-06 02:48:20 +0530

    configured for PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,71 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "./",
+  plugins: [react(), VitePWA(
+    {
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      includeAssets: ["**/*"],
+      manifest: {
+        name: "moviesdb-pwa",
+        short_name: "moviesdb-pwa",
+        theme_color: "#000000",
+        background_color: "#000000",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+
+          },
+          {
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "apple touch icon"
+          },
+          {
+            src: "/masked-icon.svg",
+            sizes: "512x512",
+            type: "image/svg",
+            purpose: "any maskable"
+          },
+          {
+            src: "/logo192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/logo256.png",
+            sizes: "256x256",
+            type: "image/png"
+          },
+          {
+            src: "/logo384.png",
+            sizes: "384x384",
+            type: "image/png"
+          },
+          {
+            src: "/logo512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    }
+  )],
 })
+
