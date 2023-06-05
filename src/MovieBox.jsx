004513bdd7551f855d92d0212a6fbd836182aceb
Gaurav Tak <98230369+gaurav-2-0-0-2@users.noreply.github.com>
2023-06-06 05:08:55 +0530

    optimized for smaller screens

--- src/MovieBox.jsx
+++ src/MovieBox.jsx
@@ -6,7 +6,7 @@ const MovieBox = ({ title, poster_path, overview, release_date }) => {
     return (
 
         <div className="bg-white p-4">
-            <img className="h-[450px] w-full mb-4" src={imageUrl + poster_path} alt="1" />
+            <img className="md:h-[450px] md:w-full mb-4" src={imageUrl + poster_path} alt="1" />
             <h1 className="text-2xl font-bold">{title}</h1>
             <p>{overview}</p>
             <p className="font-bold">Release Date:{release_date}</p>
